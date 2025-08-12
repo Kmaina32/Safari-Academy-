560ba0429b9bb4a8ca84588efc625381f60ffc08
ditman kenya <ditmankenya@gmail.com>
2025-08-12 10:50:53 +0000

    Layout fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
-
-import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import ClientAppLayout from './ClientAppLayout';
-
-export const metadata: Metadata = {
-  title: 'Safari Academy',
-  description: 'A comprehensive e-learning platform for hosting and accessing online courses.',
-};
 
 export default function RootLayout({
   children,
@@ -17,6 +10,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <title>Safari Academy</title>
+        <meta name="description" content="A comprehensive e-learning platform for hosting and accessing online courses." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
