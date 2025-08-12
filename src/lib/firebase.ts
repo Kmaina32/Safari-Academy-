77377fad53510758f5f5c0c220f33451e435509a
ditman kenya <ditmankenya@gmail.com>
2025-08-12 11:53:56 +0000

    npm install firebase and :// Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,13 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "safari-academy",
-  "appId": "1:77950866087:web:077dd6bb784e850d618182",
-  "storageBucket": "safari-academy.firebasestorage.app",
-  "apiKey": "AIzaSyCBDQDJuciLptV00kitKmf0oAXJ4OQonjI",
-  "authDomain": "safari-academy.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "77950866087"
+  apiKey: "AIzaSyAdf4on7PmsA6r1Ahb4vQmgCKxWlQAi_j0",
+  authDomain: "facefinder-xvhoj.firebaseapp.com",
+  projectId: "facefinder-xvhoj",
+  storageBucket: "facefinder-xvhoj.firebasestorage.app",
+  messagingSenderId: "993650061227",
+  appId: "1:993650061227:web:30648b8d25bcb15bfa6cee"
 };
 
 // Initialize Firebase
